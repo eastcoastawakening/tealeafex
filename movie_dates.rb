7be77aef7afc2c91abae22257b84ed63d0ced8f4
Myst_Rizing <pamela@ecawake.com>
2015-07-04 17:32:37 -0500

    updated movie dates

--- movie_dates.rb
+++ movie_dates.rb
@@ -9,3 +9,11 @@ puts movies[:anchorman]
 puts movies[:man_of_steel]
 puts movies[:a_beautiful_mind]
 puts movies[:the_evil_dead]
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
