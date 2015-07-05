c3389e8b9677387fa39573d45e1fddb08ce3f1a6
Myst_Rizing <pamela@ecawake.com>
2015-07-05 16:13:03 -0500

    using counter

---
+++ addmult.rb
@@ -0,0 +1,13 @@
+
+
+def add(num1,num2,counter=1)
+    while counter <= 5 do
+      return sum = num1 + num2
+      counter += 1
+    end
+      mult = num1 * num2
+end
+
+
+puts add(20,2)
+puts add(20,2,6)
